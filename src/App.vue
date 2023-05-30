4c09507a13ff973a671557827fd2e9b8744abdc9
Simone Carcagnì <simonecarcagni@libero.it>
2023-05-31 00:26:34 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import MyHeader from './components/MyHeader.vue';
+import MyMain from './components/MyMain.vue';
+export default {
+  components: {
+    MyHeader,
+    MyMain
+  }
+}
+</script>
+
+<template>
+  <MyHeader />
+  <MyMain />
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,17 @@
+<script>
+import ProjectCard from './ProjectCard.vue';
+export default {
+    components: {
+        ProjectCard
+    },
+    name: 'MyMain',
+}
+</script>
+
+<template>
+    <ProjectCard />
+</template>
+
+<style lang="scss">
+@use '../styles/general.scss';
+</style>
