736a72115e2e3b9c3d95b53689f1b2a733dd86ef
Simone Carcagnì <simonecarcagni@libero.it>
2023-05-31 16:17:03 +0200

    Manutenzione codice per il corretto components

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,15 +1,55 @@
 <script>
+import axios from 'axios';
 import ProjectCard from './ProjectCard.vue';
 export default {
     components: {
         ProjectCard
     },
     name: 'MyMain',
+    data() {
+        return {
+            apiUrl: 'http://localhost:8000',
+            projects: [],
+            currentPage: 1,
+            lastPage: null
+        }
+    },
+    methods: {
+        getPost(goToPage) {
+            axios.get(`${this.apiUrl}/api/projects`,
+                {
+                    params: {
+                        page: goToPage
+                    }
+                })
+                .then(response => {
+                    this.projects = response.data.results.data;
+                    this.currentPage = response.data.results.current_page;
+                    this.lastPage = response.data.results.last_page;
+                });
+        }
+    },
+    mounted() {
+        this.getPost(1);
+    }
 }
 </script>
 
 <template>
-    <ProjectCard />
+    <div class="container d-flex flex-wrap">
+        <div v-for="project in projects">
+            <ProjectCard :project="project" />
+        </div>
+
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" @click="getPost(currentPage - 1)"
+                        :class="{ 'disabled': currentPage == 1 }">Previous</a></li>
+                <li class="page-item"><a class="page-link" @click="getPost(currentPage + 1)"
+                        :class="{ 'disabled': currentPage == lastPage }">Next</a></li>
+            </ul>
+        </nav>
+    </div>
 </template>
 
 <style lang="scss">
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,57 +1,26 @@
 <script>
-import axios from 'axios';
 export default {
     name: 'ProjectCard',
     data() {
         return {
-            apiUrl: 'http://localhost:8000',
-            projects: [],
-            currentPage: 1,
-            lastPage: null
+            apiUrl: 'http://localhost:8000'
         }
     },
-    methods: {
-        getPost(goToPage) {
-            axios.get(`${this.apiUrl}/api/projects`,
-                {
-                    params: {
-                        page: goToPage
-                    }
-                })
-                .then(response => {
-                    this.projects = response.data.results.data;
-                    this.currentPage = response.data.results.current_page;
-                    this.lastPage = response.data.results.last_page;
-                });
-        }
-    },
-    mounted() {
-        this.getPost(1);
+    props: {
+        project: Object,
     }
 }
 </script>
 
 <template>
-    <div class="container d-flex flex-wrap">
-        <div class="card m-5" v-for="project in   projects " style="width: 18rem;">
-
-            <img :src="`${this.apiUrl}/storage/${project.image}`" class="card-img-top" :alt="project.title">
-            <div class="card-body">
-                <h5 class="card-title">{{ project.title }}</h5>
-                <h5>{{ project.type?.name }}</h5>
-                <p class="card-text">{{ project.description }}</p>
-                <a href="#" class="btn btn-primary">Mostra Progetto</a>
-            </div>
-
+    <div class="card m-5" style="width: 18rem;">
+        <img :src="`${this.apiUrl}/storage/${project.image}`" class="card-img-top" :alt="`${project.title}`">
+        <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <h5>{{ project.type?.name }}</h5>
+            <p class="card-text">{{ project.description }}</p>
+            <a href="#" class="btn btn-primary">Mostra Progetto</a>
         </div>
-        <nav aria-label="Page navigation example">
-            <ul class="pagination">
-                <li class="page-item"><a class="page-link" @click="getPost(currentPage - 1)"
-                        :class="{ 'disabled': currentPage == 1 }">Previous</a></li>
-                <li class="page-item"><a class="page-link" @click="getPost(currentPage + 1)"
-                        :class="{ 'disabled': currentPage == lastPage }">Next</a></li>
-            </ul>
-        </nav>
     </div>
 </template>
 
